4e1abf61088f360c6cfc0a3a51f476feec5bea75
aneishaa <shankaraneisha@gmail.com>
2021-07-05 06:49:47 +0530

    Routing - Router Module.ts and Appcomponent.html modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+const routes: Routes = [
+  {
+    path:"", component:LoginComponent
+  
+  },
+  {
+  path:"dashboard",component:DashboardComponent
 
-const routes: Routes = [];
+},
+
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
