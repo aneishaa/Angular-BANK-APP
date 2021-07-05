6fb8e5d65c7a011cdfcd253daa8e7a8d068d316e
aneishaa <shankaraneisha@gmail.com>
2021-07-05 06:04:17 +0530

    Bank App event binding

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({ 
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  Aim ="Welcome to Luminar Bank"
+  account = "Account Number Please"
+  accno = ""
+  pwd = ""
+  users:any = {
+    1000: { acno: 1000, username: "userone", password: "userone", actype: "savings", balance: 5000 },
+    1001: { acno: 1001, username: "usertwo", password: "usertwo", actype: "savings", balance: 6000 },
+    1002: { acno: 1002, username: "userthree", password: "userthree", actype: "savings", balance: 7000 },
+    1003: { acno: 1003, username: "userfour", password: "userfour", actype: "savings", balance: 8000 }
+
+}
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  login (){
+   
+    var acno = this.accno;
+    var pwd = this.pwd;
+    let accDetails = this.users;
+    if (acno in accDetails){
+      if (pwd == accDetails[acno]["password"]){
+       alert("Login Successful")
+      }
+      else{
+        alert("Incorrect Password")
+      }
+
+    }
+    else {
+      alert("Invalid Account details")
+    }
+
+
+
+
+  }
+  accChange(event:any){
+    this.accno =event.target.value
+  //  alert(this.accno)
+  }
+  passwdChange(event:any){
+    this.pwd =event.target.value
+  //  alert(this.pwd)
+  }
+}
